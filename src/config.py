c524ad291e2db83a3ee719b2bba6647c5c003b2e
trevor16gordon <trevor16gordon@gmail.com>
2022-05-12 12:35:25 -0400

    Changing config

--- src/config.py
+++ src/config.py
@@ -35,19 +35,20 @@ class TrainingConfig():
     NAME = "infogan"
     TRAIN_SAMPLE_SIZE = 2500*0.8
     VALID_SAMPLE_SIZE = 2500*0.2
-    BATCH_SIZE = 64
-    NUM_EPOCHS = 10
+    BATCH_SIZE = 32
+    NUM_EPOCHS = 100
     LEARNING_RATE_DISC = 0.0002
-    LEARNING_RATE_GEN = 0.0002
+    LEARNING_RATE_Q = 0.0002
+    LEARNING_RATE_GEN = 0.0005
     ADAM_BETA = 0.5
     RELATIVE_LOSS = 1
-    LATENT_NOISE_DIM = 30#62
+    LATENT_NOISE_DIM = 62
     IMAGE_NOISE_SCALE = 0
-    DATASET_NAME = "MNIST" # Can also be "CRYPTO_PUNK"
+    DATASET_NAME = "CRYPTO_PUNK" # Can also be "CRYPTO_PUNK"
     NUM_CATEGORIES = 10
     NUM_CONTINUOUS = 4
-    DISCRIM_TRAIN_INTERVAL = 1 #How many generator training intervals are completed every discriminator update
-    CONTIN_LOSS_SCALE = 0.1
+    DISCRIM_TRAIN_INTERVAL = 2 #How many generator training intervals are completed every discriminator update
+    CONTIN_LOSS_SCALE = 0.3
     CAT_LOSS_SCALE = 1
 
 
@@ -61,5 +62,8 @@ class ReportingConfig():
     """
     IMAGES_TO_SAVE = 10
     NUM_RAND_NOISE_VECT_PLOTS_TO_SAVE = 2
+    PRINT_EVERY = 10
+    CHECKPOINT_EVERY = 10
+    SAVE_IMAGES_EVERY = 5
     # OUTPUT_IMAGE_WIDTH = 200
     # OUTPUT_IMAGE_HEIGHT = 400
